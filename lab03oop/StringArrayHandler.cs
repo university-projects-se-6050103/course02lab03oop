011928278d7d927cddb73e46691508f4969c7b9c
Vlad Golubev <golubev.vld@gmail.com>
2015-10-15 21:22:03 +0300

    Allow set boundaries

--- lab03oop/StringArrayHandler.cs
+++ lab03oop/StringArrayHandler.cs
@@ -1,21 +1,28 @@
-﻿namespace lab03oop {
+﻿using System;
+
+namespace lab03oop {
     public class StringArrayHandler {
 
-        private string[] _stringArray;
+        private string[] _array;
+        private Tuple<int, int> _boundaries;
 
         public StringArrayHandler() {
-            _stringArray = new string[10];
+            _array = new string[10];
         }
 
-        public int Length => _stringArray.Length;
+        public StringArrayHandler(int startPosition, int endPosition) : this() {
+            _boundaries = new Tuple<int, int>(startPosition, endPosition);
+        }
+
+        public int Length => _array.Length;
 
         public string this[int index] {
             get {
-                return _stringArray[index];
+                return _array[index];
             }
 
             set {
-                _stringArray[index] = value;
+                _array[index] = value;
             }
         }
     }
