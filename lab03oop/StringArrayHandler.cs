1beed19944afc7164a02d115d27b83a893ec7f7b
Vlad Golubev <golubev.vld@gmail.com>
2015-10-15 21:07:52 +0300

    Initial commit

---
+++ lab03oop/StringArrayHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace lab03oop {
+    public class StringArrayHandler {
+
+        private string[] _stringArray;
+
+        public StringArrayHandler() {
+            _stringArray = new string[10];
+        }
+
+        public int Length => _stringArray.Length;
+
+        public string this[int index] {
+            get {
+                return _stringArray[index];
+            }
+
+            set {
+                _stringArray[index] = value;
+            }
+        }
+    }
+}
